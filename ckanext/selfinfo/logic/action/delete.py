b235682235a5e940238d66bdad9d6809cd485218
Yan Rudenko <yan.rudenko@linkdigital.com.au>
2025-05-17 11:57:20 +0200

    minor update before merge

--- ckanext/selfinfo/logic/action/delete.py
+++ ckanext/selfinfo/logic/action/delete.py
@@ -6,9 +6,11 @@ from typing import Any
 from ckanext.selfinfo import utils
 import ckan.plugins.toolkit as tk
 
+from ckan import types
+
 log = logging.getLogger(__name__)
 
 
-def selfinfo_delete_profile(context, data_dict):
+def selfinfo_delete_profile(context: types.Context, data_dict: dict[str, Any]) -> bool:
     tk.check_access("sysadmin", context, data_dict)
     return utils.selfinfo_delete_redis_key(data_dict.get("profile", ""))
